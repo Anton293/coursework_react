e67f08e9d5294e330870c3d278ed960bba058423
Anton293 <werior36@gmail.com>
2024-04-27 12:18:25 +0300

    Update contact

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import {Header, AboutUs, Products, Contacts, Footer} from "./components/"
 import {Title} from "./components/"
 
 
-//TODO: Адаптивність
-//TODO: відгуки клієнтів
-//TODO:
+// Маршрут для гугл карт https://www.google.com/maps/dir/?api=1&destination=
+
 
 function App() {
     return (
@@ -36,16 +35,15 @@ function App() {
                     </p>
                     <br/>
                     <p><h3>Консультування з технологій</h3> Наші експерти допомагають вам визначити оптимальні
-                        технологічні
-                        стратегії та вирішити складні завдання.
+                        технологічні стратегії та вирішити складні завдання.
                     </p>
                     <br/>
                 </Products>
                 <Title id="contacts-content">Контакти</Title>
                 <Contacts _filepath_business_card="business_card.png">
-                    <p>Вул. Інноваційна, 9999, м. Київ, Україна</p>
-                    <p>+380 99 123 4567</p>
-                    <p>example@gmail.com</p>
+                    <p title="Відкрити гугл карти з адресою"><a href="https://maps.google.com/?q=Вул. Інноваційна, 9999, м. Київ, Україна">Вул. Інноваційна, 9999, м. Київ, Україна</a></p>
+                    <p title="Зателефонувати"><a href="tel:+380991234567">+380 99 123 4567</a></p>
+                    <p title="Надіслати електронного листа"><a href="mailto:example@gmail.com">example@gmail.com</a></p>
                 </Contacts>
             </main>
             <Footer/>
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,5 @@ export { default as Title } from "./Title/Title";
 export { default as AboutUs } from "./AboutUs/AboutUs";
 export { default as Contacts } from "./Contacts/Contacts";
 export { default as Products } from "./Products/Products";
-
+export { default as FormFeedback } from "./FormFeedback/FormFeedback";
 
